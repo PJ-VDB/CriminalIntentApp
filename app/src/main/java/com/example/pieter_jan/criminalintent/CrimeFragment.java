3c8ada63734d2930d3b7a13b5a13f38cb2040fe0
PJ-VDB <pjvandenbrande@hotmail.com>
2016-11-19 19:14:41 +0100

    Chapter 12: Dialogs

--- app/src/main/java/com/example/pieter_jan/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/example/pieter_jan/criminalintent/CrimeFragment.java
@@ -1,9 +1,12 @@
 package com.example.pieter_jan.criminalintent;
 
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
@@ -14,6 +17,7 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -25,6 +29,11 @@ import java.util.UUID;
 public class CrimeFragment extends Fragment {
 
     private static final String ARG_CRIME_ID = "crime_id";
+    private static final String DIALOG_DATE = "DialogDate";
+
+    // A request code for the FragmentManager
+    private static final int REQUEST_DATE = 0;
+
     private Crime mCrime;
     private EditText mTitleField; // EditText for the title
     private Button mDateButton; // Button that displays the date
@@ -68,8 +77,19 @@ public class CrimeFragment extends Fragment {
 
         // the date button
         mDateButton = (Button) v.findViewById(R.id.crime_date);
-        mDateButton.setText(mCrime.getDateFormatted());
-        mDateButton.setEnabled(false); // the button is not clickable for now; will be changed in Chapter 13
+        updateDate(mCrime.getDateFormatted());
+//        mDateButton.setEnabled(false); // the button is not clickable for now; will be changed in Chapter 13
+        mDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager manager = getFragmentManager();
+                DatePickerFragment dialog = DatePickerFragment.newInstance(mCrime.getDate());
+                // Tell the FragmentManager that the DatePickerFragment is linked to the CrimeFragment
+                // This is needed when passing around data
+                dialog.setTargetFragment(CrimeFragment.this, REQUEST_DATE);
+                dialog.show(manager, DIALOG_DATE);
+            }
+        });
 
         // the checkbox
         mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
@@ -84,6 +104,26 @@ public class CrimeFragment extends Fragment {
 
         return v;
     }
+
+    // Get the date from the DatePicker and bind it to the crime
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode != Activity.RESULT_OK){
+            return;
+        }
+
+        if (requestCode == REQUEST_DATE){
+            Date date = (Date) data.getSerializableExtra(DatePickerFragment.EXTRA_DATE);
+            mCrime.setDate(date); // Set the date on the crime
+            updateDate(mCrime.getDate().toString());
+        }
+    }
+
+    // Set the date to the text button
+    private void updateDate(String text) {
+        mDateButton.setText(text); // Show in on the DateButton
+    }
+
 
     // Create a bundle with the arguments that are connected to the CrimeFragment;
     // Has to happen after the Fragment gets created but Before it is added to the activity
